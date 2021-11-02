15ecf81e032d336af25eab671a499ba2fe866e51
gabrielnrt <gabrielpv98@gmail.com>
2021-11-02 20:25:02 -0300

    Alterações no código e compilação de dados em menos arquivos

--- Graduação/F896/monografia.py
+++ Graduação/F896/monografia.py
@@ -1,25 +1,23 @@
 from numpy import sqrt, array, log, linspace, pi
 from pylab import plot, scatter, show, figure, xlabel, ylabel, legend, grid, yscale, errorbar
-from pandas import read_csv
+from pandas import read_csv, read_excel
 
 hc2 = 3.89379 * (10**8)
 alpha2 = 137**(-2)
 
 # Parte 1 ------------------------------------------------------------------------------
 
-jade = read_csv('Jadee.csv',delimiter = ';', decimal = ',')
-pluto = read_csv('Pluto.csv',delimiter = ';', decimal = ',')
-tasso = read_csv('Tasso.csv', delimiter = ';', decimal = ',')
-markj = read_csv('MarkJ.csv',delimiter = ';', decimal = ',')
+planilha = read_excel('Parte1.xlsx', sheet_name = None)
 
 x = linspace(5,50,1000)
 y = (4*pi*hc2*alpha2/3)*(x**(-2))
 
 plot(x,y, color='navy', label='Previsão do Modelo')
-scatter(jade['x'],jade['y'], color='gold', label='JADE')
-scatter(pluto['x'],pluto['y'], color='red', label='PLUTO')
-scatter(tasso['x'],tasso['y'], color='lime', label='TASSO')
-scatter(markj['x'],markj['y'], color='violet', label='MARK-J')
+
+scatter(planilha['Jade']['x'],planilha['Jade']['y'], color='gold', label='JADE')
+scatter(planilha['Pluto']['x'],planilha['Pluto']['y'], color='red', label='PLUTO')
+scatter(planilha['Tasso']['x'],planilha['Tasso']['y'], color='lime', label='TASSO')
+scatter(planilha['MarkJ']['x'],planilha['MarkJ']['y'], color='violet', label='MARK-J')
 
 
 yscale('log')
@@ -31,14 +29,14 @@ show()
 
 # Parte 2 --------------------------------------------------------------------------------
 
-barber = read_csv('barber.csv', delimiter = ';', decimal = ',')
-erro = array([.3,.4,.15,.05,.04,.035,.07,.04,.045,.04,.05])
+barber = read_csv('barber.csv')
+
 
 y = (10**(-3))*(4*pi*hc2*alpha2/3)*(x**(-2))
 
 plot(x,y, color='navy', label='Previsão do Modelo')
 
-errorbar(barber['x'], barber['y'], yerr=erro, xerr=None, fmt='o', capsize=4, color='orange', label='Barber')
+errorbar(barber['x'], barber['y'], yerr=barber['Erro y'], xerr=None, fmt='o', capsize=4, color='orange', label='Barber')
 
 
 yscale('log')
@@ -63,16 +61,16 @@ def b(s,m):
     x = beta(s,m)*(3-beta(s,m)**2)
     return x
 
-razao = read_csv('razao.csv', delimiter = ';', decimal = ',')
-errox = array([0,0,0,0,0,0.02,0.04,0.04,0.04,0.08,0.02,0.1,0.23])
-erroy = array([0.005,0.01,0.007,0.01,0.01,0.006,0.006,0.012,0.008,0.015,0.015,0.017,0.011])
+# razao = read_csv('razao.csv', delimiter = '\t', decimal = ',')
+razao = read_csv('razao.csv')
+
 
 x = linspace(3.56,5,500)
 fator = 6
 y = b(x,m_tau)/(fator*b(x,M))
 
 plot(x,y, color='navy', label='Previsão do Modelo')
-errorbar(razao['x'], razao['y'], yerr=erroy, xerr=errox, fmt='o', capsize=3, color='lime', label='Dados Experimentais')
+errorbar(razao['x'], razao['y'], yerr=razao['Erro y'], xerr=razao['Erro x'], fmt='o', capsize=3, color='lime', label='Dados Experimentais')
 
 grid(True, linestyle='--')
 legend(loc=4)
